81790b515f6b2817bfedcc8fcb92a25b8055c561
soyeon-noh <1010qwerty12@gmail.com>
2021-08-06 11:09:52 +0900

    ignore

---
+++ App02_Library/app/src/main/java/com/noso/library/config/Naver_Sample.java
@@ -0,0 +1,8 @@
+package com.noso.library.config;
+
+// Naver Open API 연동을 위한 ID와 비밀번호
+// 뭐지
+public class Naver_Sample {
+    public static String CLIENT_ID = "YOUR NAVER CLIENT ID";
+    public static String CLIENT_SECRET = "YOUR NAVER CLIENT SECRET";
+}
