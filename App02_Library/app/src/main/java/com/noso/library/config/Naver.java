eb151fa76f11acaa8fd80201f28ddb7f71fd584b
soyeon-noh <1010qwerty12@gmail.com>
2021-08-06 11:05:35 +0900

    NaverSecret

--- App02_Library/app/src/main/java/com/noso/library/config/Naver.java
+++ App02_Library/app/src/main/java/com/noso/library/config/Naver.java
@@ -1,5 +1,6 @@
 package com.noso.library.config;
 
+// Naver Open API 연동을 위한 ID와 비밀번호
 public class Naver {
     public static String CLIENT_ID = "YOUR NAVER CLIENT ID";
     public static String CLIENT_SECRET = "YOUR NAVER CLIENT SECRET";
